d212600f30aed3bbb3fe74dd8f9aa0235e8fef28
Eddie Dunn <eddie@eddiedunn.com>
2023-05-19 15:25:49 -0400

    Added code to create database and load extension

--- load_data.py
+++ load_data.py
@@ -15,6 +15,8 @@ import torch
 from InstructorEmbedding import INSTRUCTOR
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 # pip install python-dotenv openai langchain InstructorEmbedding pypdf pgvector psycopg2-binary torch torchvision torchaudio sentence_transformers
 # load multiple pdfs recursively from a directory into postgres using InstructorEmbeddings.
@@ -22,6 +24,60 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 torch.cuda.empty_cache()
 load_dotenv()
 
+
+#----------------------------------------------------------------------#
+#
+#   First we need to create the database and load the vector extension
+#   If it needs to be done
+#
+#----------------------------------------------------------------------#
+
+# Get database parameters
+database = os.getenv('PGVECTOR_DATABASE')
+user = os.getenv('PGVECTOR_USER')
+password = os.getenv('PGVECTOR_PASSWORD')
+host = os.getenv('PGVECTOR_HOST')
+port = os.getenv('PGVECTOR_PORT')
+
+# Connect to the PostgreSQL server
+conn = psycopg2.connect(
+    dbname="postgres",  # connect to the default database to execute administrative commands
+    user=user,
+    password=password,
+    host=host,
+    port=port
+)
+
+# Set the isolation level to AUTOCOMMIT to execute the CREATE DATABASE command
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+# Create a new database if it does not exist
+with conn.cursor() as cur:
+    cur.execute(f"CREATE DATABASE {database} WITH OWNER {user} ENCODING 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8' TEMPLATE template0;")
+    conn.commit()
+
+# Connect to the newly created database
+conn = psycopg2.connect(
+    dbname=database,
+    user=user,
+    password=password,
+    host=host,
+    port=port
+)
+
+# Create the vector extension if it does not exist
+with conn.cursor() as cur:
+    cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+    conn.commit()
+
+# Close the connection
+conn.close()
+
+#----------------------------------------------------------------------#
+#
+#   Next we need to load the documents and create the embeddings
+#
+#----------------------------------------------------------------------#
 loader = DirectoryLoader(
     os.getenv('PDF_ROOT'),
     glob="./*.pdf",
